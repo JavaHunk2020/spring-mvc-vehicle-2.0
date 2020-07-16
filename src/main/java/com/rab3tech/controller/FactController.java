16385529beb97adc9445b439dd70ad17782b1886
JavaHunk2020 <javahunk2020@gmail.com>
2020-07-16 05:36:15 +0530

    Spring MVC Day 1 + Factorial Program

---
+++ src/main/java/com/rab3tech/controller/FactController.java
@@ -0,0 +1,24 @@
+package com.rab3tech.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//Here we can use only this annotation
+@Controller
+public class FactController {
+	
+	//doGet
+	@GetMapping("/auth")
+	public String calFact(HttpServletRequest req){
+		String num=req.getParameter("num");
+		int suma=1;
+		for(int x=2;x<=Integer.parseInt(num);x++){
+			suma=suma*x;
+		}
+		req.setAttribute("resulta", suma);
+		return "fact";  // /fact.jsp
+	}
+
+}
